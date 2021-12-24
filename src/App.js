3f641291560569e8ce7c61603a3393dc53b1913a
Harold <m.h.gamze@gmail.com>
2021-12-24 13:37:22 +0300

    task.js added in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {View,Text, StyleSheet} from 'react-native';
 import Headline from "./components/Headline";
+import Task from "./components/Task";
 
 const App = () => {
   return(
     <View style={styles.container}>
       <Headline/>
+      <Task/>
       
 
     </View>
